4a6914f04e898fe1b4bff278cc32ee46a1ec3ce3
abcdragon <heybrostop@gmail.com>
2020-11-22 01:39:47 +0900

    섹션 별로 헤더 추가

--- texttab.py
+++ texttab.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QPushButton, QTreeWidget, QTreeWidgetItem
 from PyQt5.QtCore import Qt
 
@@ -11,26 +11,44 @@ class TextTab(QWidget):
         super().__init__()
 
         self.class_list = QTreeWidget()
+        self.class_list.setColumnCount(5)
         self.class_list.setHeaderHidden(True)
 
         self.add_line = QPushButton('새로운 클래스 추가하기')
         self.add_line.clicked.connect(self.add_line_clicked)
 
+        self.to_file = QPushButton('템플릿 코드 생성하기')
+
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.add_line)
+        button_layout.addWidget(self.to_file)
+
         layout = QVBoxLayout()
         layout.addWidget(self.class_list)
-        layout.addWidget(self.add_line, alignment=Qt.AlignBottom)
+        layout.addLayout(button_layout)
         self.setLayout(layout)
 
     def create_item(self, class_info):
         root = QTreeWidgetItem()
         root.setText(0, class_info['name'])
 
-        item_roots = [('method', QTreeWidgetItem(root)), ('variable', QTreeWidgetItem(root))]
+        item_roots = [('method', QTreeWidgetItem(root)),
+                      ('variable', QTreeWidgetItem(root))]
 
-        for element, item_root in item_roots:
-            item_root.setText(0, element)
-            new_method_item = QTreeWidgetItem(item_root)
-            new_method_item.setText(0, class_info[element]['name'])
+        for key, item_root in item_roots:
+            item_root.setText(0, key)
+
+            if not class_info[key]:
+                continue
+
+            header = QTreeWidgetItem(item_root)
+            for index, (header_text, _) in enumerate(class_info[key][0]):
+                header.setText(index + 1, header_text)
+
+            new_item = QTreeWidgetItem(item_root)
+            for info in class_info[key]:
+                for index, (_, content) in enumerate(info):
+                    new_item.setText(index + 1, content)
 
         return root
 
