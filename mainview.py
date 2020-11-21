8cfa9879fe05867e5ca10a685f45df9396b75ea5
abcdragon <heybrostop@gmail.com>
2020-11-21 21:20:38 +0900

    프로그램의 메인 뷰

---
+++ mainview.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import QWidget, QTabWidget
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QLabel
+
+from texttab import TextTab
+
+
+class MainView(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        tab = QTabWidget()
+        tab.addTab(TextTab(), '텍스트')
+        tab.addTab(QLabel('UML'), 'UML')
+
+        layout = QHBoxLayout()
+        layout.addWidget(tab)
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    main = MainView()
+    main.show()
+    sys.exit(app.exec_())
