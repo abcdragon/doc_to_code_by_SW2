b5f31ea1891dbc50d08507cbb06d875eff3639c7
abcdragon <heybrostop@gmail.com>
2020-11-25 14:19:13 +0900

    데이터 구조 변경에 따른 수정

--- doctocode.py
+++ doctocode.py
@@ -1,29 +1,40 @@
 class DocToCode:
     def __init__(self, root):
+        tab = lambda w: ' ' * 4 * w
+
         with open('new_file.py', 'w') as file:
             for obj in root:
-                _class = obj['class']
-                _class_declare = 'class ' + _class['name']
+                print(root)
+                _class = obj.data['class'][0]
+                _class_declare = 'class ' + _class[0]
 
-                if _class['parent']:
-                    _class_declare += '(%s)' % _class['parent']
+                if _class[1]:
+                    _class_declare += '(%s)' % _class[1]
 
                 _class_declare += ':\n'
                 file.write(_class_declare)
 
-                value = obj['variable']
-                if value['name']:
-                    file.write(' ' * 4 + 'def __init__(self):\n')
-                    file.write(' ' * 8 + 'self.%s = %s\n' % (value['name'], value['inital value']))
-                file.write('\n')
+                _variable = obj.data['variable']
+                _variable_declare = tab(1) + 'def __init__(self):\n'
 
-                method = obj['method']
-                if not method['name']:
-                    file.write('    pass\n\n')
+                for var_info in _variable:
+                    if var_info[0]:
+                        _variable_declare += tab(2) + 'self.%s = ' % var_info[0]
+                        _variable_declare += ("'%s'\n" if var_info[1] == 'str' else '%s') % var_info[2]
 
-                else:
-                    file.write(' ' * 4 + 'def %s(%s):\n' % (method['name'], method['parameter']))
-                    file.write(' ' * 8 + 'pass\n')
-                file.write('\n\n')
+                if '=' not in _variable_declare:
+                    _variable_declare += tab(2) + 'pass\n\n'
 
+                file.write(_variable_declare)
 
+                _method = obj.data['method']
+                _method_declare = ''
+                for method_info in _method:
+                    if method_info[0]:
+                        _method_declare += tab(1) + 'def %s(self' % method_info[0]
+                        if method_info[1]:
+                            _method_declare += ', %s' % method_info[1]
+
+                        _method_declare += '):\n%spass\n\n' % tab(2)
+
+                file.write(_method_declare + '\n')
