303fe86789db4800405d80174e04e8118a99469f
abcdragon <heybrostop@gmail.com>
2020-11-25 14:17:18 +0900

    amend 함수 제거

--- classinfomodel.py
+++ classinfomodel.py
@@ -7,10 +7,13 @@ class DataModel:
         }
 
         self.data = {ele: [[''] * len(h)] for ele, h in self.header.items()}
-        print('DataModel', self.data)
+        self.data['variable'][0][1] = 'str'
 
     def add(self, ele):
         new_datum = [''] * len(self.header[ele])
+        if ele == 'variable':
+            new_datum[1] = 'str'
+
         self.data[ele].append(new_datum)
 
     def remove(self, element, row):
