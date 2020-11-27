c4428c69cb1c95dcd798ade352892c311c69f3e6
abcdragon <heybrostop@gmail.com>
2020-11-28 02:56:16 +0900

    클래스/메서드/변수 이름 중복 체크, 메서드 매개변수 중복 체크

--- class_treeview.py
+++ class_treeview.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QMessageBox
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
 from PyQt5.QtWidgets import QPushButton, QTreeWidget, QTreeWidgetItem
 
@@ -7,8 +7,11 @@ from doctocode import DocToCode
 
 
 class ClassTreeView(QWidget):
-    def __init__(self):
+    def __init__(self, pre_data=None):
         super().__init__()
+
+        self.all_data = pre_data if pre_data else []
+
         self.init_ui()
 
     def init_ui(self):
@@ -16,9 +19,9 @@ class ClassTreeView(QWidget):
         self.class_list.setColumnCount(5)
         self.class_list.setHeaderHidden(True)
         self.class_list.itemDoubleClicked.connect(self.change_item_content)
-
         for data in self.all_data:
             self.class_list.addTopLevelItem(self.create_item(data))
+            self.class_list.scrollToBottom()
 
         self.add_item = QPushButton('새로운 클래스 추가하기')
         self.add_item.clicked.connect(self.add_item_clicked)
@@ -40,17 +43,56 @@ class ClassTreeView(QWidget):
         self.setLayout(layout)
 
     def add_item_clicked(self):
-        info = ClassInfoDialog()
-        info.exec_()
+        info = None
+        while True:
+            info = ClassInfoDialog(info.data if info else None)
+            info.exec_()
 
-        print(info.data)
-        if not info.data:
-            return
+            if not info.success:
+                break
 
-        self.all_data.append(info.data)
-        new_item = self.create_item(info.data)
-        self.class_list.addTopLevelItem(new_item)
-        self.class_list.scrollToBottom()
+            box = QMessageBox()
+            box.setWindowTitle('경고')
+            box.setModal(True)
+
+            # 클래스 이름이 같은게 있으면 continue
+            if [0 for data in self.all_data if data.data['class'][0][0] == info.data.data['class'][0][0]]:
+                box.setText('클래스의 이름이 겹쳤습니다.')
+                box.exec_()
+
+                continue
+
+            # 메서드 이름이 같은게 있으면 강제 종료해서 while 처음으로 돌아가고,
+            for info_name, info_para, _ in info.data.data['method']:
+                if [0 for data in self.all_data for name, _, _ in data.data['method'] if name == info_name]:
+                    box.setText('메서드의 이름이 겹치는 요소가 있습니다.')
+                    box.exec_()
+                    break
+
+                tmp = [c for c in info_para.replace(',', ' ').split(' ') if c]
+                if len(tmp) != len(set(tmp)):
+                    box.setText('매개변수 중 겹치는 것이 있습니다.')
+                    box.exec_()
+                    break
+
+            # 메서드 이름이 겹치지 않고, 매개변수도 안 겹친다면 변수에 대해 검색
+            else:
+                # 변수 이름이 같은게 있으면 강제 종료해서 while 처음으로 돌아가고
+                for info_name, _, _ in info.data.data['variable']:
+                    if [0 for data in self.all_data for name, _, _ in data.data['variable'] if name == info_name]:
+                        box.setText('변수의 이름이 겹치는 요소가 있습니다.')
+                        box.exec_()
+
+                        break
+
+                # 변수 이름까지 같은게 없으면 break으로 while문 탈출
+                else:
+                    break
+
+        if info.success:
+            self.all_data.append(info.data)
+            self.class_list.addTopLevelItem(self.create_item(info.data))
+            self.class_list.scrollToBottom()
 
     def del_item_clicked(self):
         item = self.class_list.selectedItems()[0]
@@ -74,7 +116,7 @@ class ClassTreeView(QWidget):
         info = ClassInfoDialog(self.all_data[index])
         info.exec_()
 
-        if not info.data:
+        if not info.success:
             return
 
         self.all_data[index] = info.data
